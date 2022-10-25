7b0a3bd9f88e7ac7581d64e3aeef17433b8e6617
Nhan <thanhnhanphan2888@gmail.com>
2022-10-25 09:16:59 +0700

    case study 12345

--- src/ss17_lap_trinh_huong_doi_tuong_02/bai_tap/game_don_gian/objects-master/script.js
+++ src/ss17_lap_trinh_huong_doi_tuong_02/bai_tap/game_don_gian/objects-master/script.js
@@ -3,6 +3,9 @@
  */
 let maxWidth = 1000;
 let maxHeight = 500;
+let a = 0;
+var i;
+
 function Hero(image, top, left, size) {
     this.image = image;
     this.top = top;
@@ -16,39 +19,48 @@ function Hero(image, top, left, size) {
             ' style="top: ' + this.top + 'px; left:' + this.left + 'px; position:absolute;" />';
     }
 
-    this.moveRight = function (direction) {
-        if (direction === "d"){
-            this.left += 20;
-        }
+    this.moveRight = function () {
+        this.left += 20;
     }
-    this.moveLeft = function (direction) {
-        if (direction === "a"){
-            this.left -= 20;
-        }
+    this.moveLeft = function () {
+        this.left -= 20;
     }
-    this.moveTop = function (direction) {
-        if (direction === "w"){
-            this.top -= 20;
-        }
+    this.moveTop = function () {
+        this.top -= 20;
     }
-    this.moveBottom = function (direction) {
-        if (direction === "s"){
-            this.top += 20;
-        }
+    this.moveBottom = function () {
+        this.top += 20;
     }
-
 }
 
 var hero = new Hero('nobita.png', 20, 30, 200);
 
 function start() {
-    let a = document.getElementById("move").value;
-    hero.moveRight(a);
-    hero.moveLeft(a);
-    hero.moveTop(a);
-    hero.moveBottom(a);
-    document.getElementById("move").value = "";
-    document.getElementById('game').innerHTML = hero.getHeroElement();
-    setTimeout(start, 1);
+    a = Math.ceil(Math.random() * 5);
+
+    switch (a) {
+        case 0:
+            hero.moveRight();
+            document.getElementById("game").innerHTML = hero.getHeroElement();
+            break;
+        case 1:
+            hero.moveLeft();
+            document.getElementById("game").innerHTML = hero.getHeroElement();
+            break;
+        case 2:
+            hero.moveTop();
+            document.getElementById("game").innerHTML = hero.getHeroElement();
+            break;
+        case 3:
+            hero.moveBottom();
+            document.getElementById("game").innerHTML = hero.getHeroElement();
+            break;
+        default:
+            hero.moveRight();
+            document.getElementById("game").innerHTML = hero.getHeroElement();
+            break;
+    }
+    setTimeout(start, 300);
 }
+
 start();
